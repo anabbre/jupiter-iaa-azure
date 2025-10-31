229bc95ae3f2b7cfea23aaa653d2dbb25f0c13d8
Ana Belén Ballesteros <anabbre@hotmail.com>
2025-10-31 13:33:13 +0100

    fix(terraform): vnet/subnets usan RG existente + tags; matrix no cancel

--- docs/terraform/vnet_subnets.tf
+++ docs/terraform/vnet_subnets.tf
@@ -1,25 +1,21 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-jupiter-demo"
-  location = "West Europe"
-}
-
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-jupiter"
-  address_space       = ["10.0.0.0/16"]
+  name                = "${var.name_prefix}-vnet"
+  address_space       = ["10.10.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.common_tags
 }
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet1"
+  name                 = "${var.name_prefix}-snet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.10.1.0/24"]
 }
 
 resource "azurerm_subnet" "subnet2" {
-  name                 = "subnet2"
+  name                 = "${var.name_prefix}-snet2"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.10.2.0/24"]
 }

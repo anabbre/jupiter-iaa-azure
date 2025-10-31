b35e03d0d29ff689586056b3ba7f7a13dedd2abd
Ana Belén Ballesteros <anabbre@hotmail.com>
2025-10-31 13:27:30 +0100

    fix(terraform): quitar AWS y ajustar static website en Azure

---
+++ docs/terraform/storage_acount.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.name_prefix}-rg"
+  location = var.location
+  tags     = var.common_tags
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = replace("${var.name_prefix}sa", "-", "")
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  allow_blob_public_access = false
+  min_tls_version          = "TLS1_2"
+
+  tags = var.common_tags
+}

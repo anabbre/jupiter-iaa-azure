40a25191a0efb201cd08cb1b0afd6459f5bbd13a
Ana Belén Ballesteros <anabbre@hotmail.com>
2025-10-31 13:14:36 +0100

    style(terraform): fmt en docs/*

--- docs/terraform/outputs.tf
+++ docs/terraform/outputs.tf
@@ -1,5 +1,5 @@
 # outputs.tf
 output "website_url" {
-  value = aws_s3_bucket.static_site.website_endpoint
+  value       = aws_s3_bucket.static_site.website_endpoint
   description = "URL of the static website"
 }
